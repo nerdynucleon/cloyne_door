4615f814a713941c6824089780d7439b986565dd
nerdynucleon <alexychong@gmail.com>
2017-11-10 01:31:39 +0000

    added date and time logging, cleaned up some commented code

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import serial
 import RPi.GPIO as GPIO
 import time
+import datetime
 import logging
 
 #Initialize LogFile
-logging.basicConfig(filename='door.log',level=logging.DEBUG)
+logging.basicConfig(filename='/root/cloynefrontdoor.log',level=logging.DEBUG)
 
 # Set System Time
 import subprocess
-#import os
 try:
 	import ntplib
 	client = ntplib.NTPClient()
 	response = client.request('pool.ntp.org')
 	date_time = subprocess.check_output('date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)), shell=True)
-	#date_time = os.system('date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)))
 	logging.info(date_time)
 except:
 	logging.info('Failed to Set System Time')
@@ -24,6 +23,7 @@ except:
 #GPIO drives optoisolator which in turns opens door
 optoisolator_pin = 2
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setwarnings(False)
 GPIO.setup(optoisolator_pin, GPIO.OUT) # OptoIsolator pin set as output
 GPIO.output(optoisolator_pin, GPIO.LOW)# Initial state for Optoisolator : closed
 
@@ -32,6 +32,7 @@ HID = serial.Serial('/dev/ttyUSB0') # Open port at 9600,8,N,1, no timeout:
 
 def open_door():
 	# Turn optoisolator on
+	print 'Opening Door!'
 	GPIO.output(optoisolator_pin, GPIO.HIGH)
 	time.sleep(0.5)
 	GPIO.output(optoisolator_pin, GPIO.LOW)
@@ -45,6 +46,8 @@ def calculate_id(hex_string):
 	return id_value
 
 def valid_ID(id_num):
+	# TO DO CREATE DATABASE TO CHECK
+	datetime.datetime.now() # check if valid for this academic semester
 	return id_num > 0
 
 def main():
@@ -53,8 +56,8 @@ def main():
 		id_num = calculate_id(hex_string)
 		if( valid_ID(id_num)):
 			open_door()
-			logging.info([hex_string, id_num, 'GRANTED'])
+			logging.info([hex_string, id_num, 'GRANTED', datetime.datetime.now()])
 		else:
-			logging.info([hex_string, id_num, 'DENIED'])
+			logging.info([hex_string, id_num, 'DENIED', datetime.datetime.now()])
 	
 main()
